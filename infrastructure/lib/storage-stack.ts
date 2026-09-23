e25648ae3bff725f5b7c9e55296936e0cc6d2a48
S_Kono <siiuiaemtkki@gmail.com>
2026-09-23 22:28:05 +0900

    【PoC実装】Raw Data S3の構築

--- infrastructure/lib/storage-stack.ts
+++ infrastructure/lib/storage-stack.ts
@@ -1,8 +1,28 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { BlockPublicAccess, Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class StorageStack extends Stack {
+  public readonly rawDataBucket: Bucket;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const environment = this.node.tryGetContext('environment') as string;
+
+    this.rawDataBucket = new Bucket(this, 'RawDataBucket', {
+      bucketName: `stream-insight-${environment}-raw-${this.account}`,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      encryption: BucketEncryption.S3_MANAGED,
+      lifecycleRules: [
+        {
+          id: 'DeleteAfter7Days',
+          enabled: true,
+          expiration: Duration.days(7),
+        },
+      ],
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
   }
 }
